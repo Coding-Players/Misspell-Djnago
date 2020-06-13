b3d6b093cc32685b8e59c471b7cf94c20f2f8542
Coding-Players <ataota.com@gmail.com>
2020-06-13 16:08:58 +0530

    commit 13/06/2020 at 4:8

--- CapMap/models.py
+++ CapMap/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class MapLocation(models.Model):
+    latitude = models.FloatField(max_length=18)
+    longitude = models.FloatField(max_length=18)
+
+    def __float__(self):
+        return "Latitude:" + self.latitude + " Longitude:" + self.longitude
--- CapMap/views.py
+++ CapMap/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import MapLocation
 
 
 def map_view(request):
-    return HttpResponse('Map Page')
+    key = str('AIzaSyCw6G6AnBOLTw1tcl3n4sxxvIPZTqO2khQ')
+    all_location = MapLocation.objects.all()
+    print(all_location)
+    # for location in all_location:
+    #     print(location.latitude)
+    if request.method == 'POST':
+        lat = request.POST["latitude"]
+        lng = request.POST["longitude"]
+        # print(f"Latitude: {lat}\nLongitude: {lng}")
+        hunter1 = float(lat)
+        hunter2 = float(lng)
+        add_lat = MapLocation(latitude=hunter1, longitude=hunter2)
+        add_lat.save()
+        print(add_lat)
+        # print(hunter1, hunter2)
+
+    return render(request, 'CapMap/MapPage.html', {
+            'key': key,
+            'all_location': all_location}
+         )
+
 
